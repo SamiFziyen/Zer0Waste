a71645807e50d546107d41f4c7bd5267ca4b1db5
NEO400 <120325099+NEO400@users.noreply.github.com>
2023-04-03 02:40:57 +0200

    Add comments to code

--- js/script.js
+++ js/script.js
@@ -1,77 +1,98 @@
+// Define myName and h1 elements and log their values
 const myName = "Sami Fziyen";
 const h1 = document.querySelector(".heading-primary");
 console.log(myName);
 console.log(h1);
 
-///////////////////////////////////////////////////////////
-// Set current year
+// Add event listener to h1 element to change its text content and style on click (commented out for now)
+// h1.addEventListener("click", function () {
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
+
+// Set current year on yearEl element
 const yearEl = document.querySelector(".year");
-yearEl.textContent = new Date().getFullYear();
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
 
-///////////////////////////////////////////////////////////
-// Make mobile navigation work
-
+// Make mobile navigation work by toggling nav-open class on headerEl element when btnNavEl is clicked
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
 
-btnNavEl.addEventListener("click", () => headerEl.classList.toggle("nav-open"));
-
-///////////////////////////////////////////////////////////
-// Smooth scrolling animation
-
+// Add smooth scrolling animation to all links with a href attribute
 const allLinks = document.querySelectorAll("a:link");
-
-allLinks.forEach(link => {
-  link.addEventListener("click", e => {
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
     e.preventDefault();
     const href = link.getAttribute("href");
 
-    // Scroll back to top
-    if (href === "#") window.scrollTo({ top: 0, behavior: "smooth" });
+    // If href is "#" scroll back to top with smooth behavior
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
 
-    // Scroll to other links
+    // If href starts with "#" scroll to the section with the matching ID with smooth behavior
     if (href !== "#" && href.startsWith("#")) {
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
 
-    // Close mobile naviagtion
-    if (link.classList.contains("main-nav-link")) headerEl.classList.toggle("nav-open");
+    // If link has main-nav-link class, toggle nav-open class on headerEl to close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
   });
 });
 
-///////////////////////////////////////////////////////////
-// Sticky navigation
-
+// Make header sticky when scrolling down by adding or removing sticky class on document.body based on sectionHeroEl visibility
 const sectionHeroEl = document.querySelector(".section-hero");
-
 const obs = new IntersectionObserver(
-  entries => {
+  function (entries) {
     const ent = entries[0];
 
-    if (ent.isIntersecting === false) document.body.classList.add("sticky");
-    if (ent.isIntersecting === true) document.body.classList.remove("sticky");
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
   },
   {
     root: null, // In the viewport
     threshold: 0,
-    rootMargin: "-80px",
+    rootMargin: "-80px", // Trigger sticky class 80px before sectionHeroEl reaches the top of the viewport
   }
 );
 obs.observe(sectionHeroEl);
 
-///////////////////////////////////////////////////////////
-// Fixing flexbox gap property missing in some Safari versions
+// Check if browser supports flexbox gap property by creating a div element with flexbox and row gap, adding two child div elements, and checking if its scrollHeight is 1
 function checkFlexGap() {
-  const flex = document.createElement("div");
-  flex.style.cssText = "display: flex; flex-direction: column; row-gap: 1px";
-  flex.innerHTML = "<div></div><div></div>";
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
 
   document.body.appendChild(flex);
-  const isSupported = flex.scrollHeight === 1;
-  document.body.removeChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
   console.log(isSupported);
 
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+  // If flexbox gap is not supported, add no-flexbox-gap class on document.body
+  if (!isSupported) {
+    document.body.classList.add("no-flexbox-gap");
+  }
 }
 checkFlexGap();
+
+// Add CSS rules for no-flexbox-gap class to fix missing flexbox
